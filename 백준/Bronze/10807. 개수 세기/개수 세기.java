667d472987c9af303c9b66cbb21714e6a8db954f
yayoung92 <140130695+yayoung92@users.noreply.github.com>
2024-01-11 18:30:08 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,23 +1,29 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
         int[] z = new int[n];
         int count = 0;
-
-        for(int i=0; i<n; i++){
-            z[i] = scan.nextInt();
-        }
         
-        int y = scan.nextInt();
-        for(int i=0; i<z.length; i++) {
+        st = new StringTokenizer(br.readLine());
+        
+        for(int i=0; i<n; i++){
+            z[i] = Integer.parseInt(st.nextToken());
+        }
+        int y = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<z.length; i++){
             if(z[i]==y){
                 count++;
             }
         }
-
         System.out.println(count);
+        br.close();
+        bw.close();
     }
 }
